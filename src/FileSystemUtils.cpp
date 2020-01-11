e31b42c26a2369a30f0e9ae8d2ef09d951c9fde9
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2020-01-11 12:38:13 +0100

    Replace PHYSFS_getUserDir with PHYSFS_getPrefDir

--- src/FileSystemUtils.cpp
+++ src/FileSystemUtils.cpp
@@ -141,21 +141,8 @@ std::vector<std::string> FILESYSTEM_getLevelDirFileNames()
 
 void PLATFORM_getOSDirectory(char* output)
 {
-#if defined(__linux__)
-	const char *homeDir = getenv("XDG_DATA_HOME");
-	if (homeDir == NULL)
-	{
-		strcpy(output, PHYSFS_getUserDir());
-		strcat(output, ".local/share/VVVVVV/");
-	}
-	else
-	{
-		strcpy(output, homeDir);
-		strcat(output, "/VVVVVV/");
-	}
-#elif defined(__APPLE__)
-	strcpy(output, PHYSFS_getUserDir());
-	strcat(output, "Library/Application Support/VVVVVV/");
+#if defined(__linux__) || defined(__APPLE__)
+	strcpy(output, PHYSFS_getPrefDir("distractionware", "VVVVVV"));
 #elif defined(_WIN32)
 	SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, output);
 	strcat(output, "\\VVVVVV\\");
